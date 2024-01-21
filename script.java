07a57f78a4b15a51d791201b52876032e889be0e
adr4an <ad4antoh@gmail.com>
2024-01-21 20:48:07 +0800

    Fixed

--- script.java
+++ script.java
@@ -3,7 +3,10 @@ public class script {
 
     public static void main(String[] args) {
         
-        System.out.println("Printed");
+        System.out.println("Machine");
+        System.out.println("Has");
+        System.out.println("A");
+        System.out.println("Problem");
 
     }
 
