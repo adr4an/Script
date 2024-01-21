3e90bc4eb8abb94e983f7c296ad20816194379af
adr4an <ad4antoh@gmail.com>
2024-01-21 21:05:07 +0800

    bug2

--- script.java
+++ script.java
@@ -3,11 +3,12 @@ public class script {
 
     public static void main(String[] args) {
         
-        System.out.println("Machine");
-        System.out.println("Has");
-        System.out.println("A");
-        System.out.println("Problem");
+        func();
+ 
+    }
 
+    public static func(){
+        System.out.println("Error message.. ");
     }
 
 }
