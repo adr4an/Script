9a7b5397c141f76d8ca3709a1c0d216412468b4c
adr4an <ad4antoh@gmail.com>
2024-01-21 21:26:31 +0800

    Subtraction

--- script.java
+++ script.java
@@ -3,12 +3,20 @@ public class script {
 
     public static void main(String[] args) {
         
-        add(5, 10);
+        int a = add(5, 10);
+        int b = sub(10, 5);
+
+        System.out.println(a);
+        System.out.println(b);
  
     }
 
     public static int add(int a, int b){
         return a + b;
+    }
+
+    public static int sub(int a, int b){
+        return a - b;
     }
 
 }
